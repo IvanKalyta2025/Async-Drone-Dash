39a8cdf348a331ef3bfff222ed6c08e0a0894a72
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-11-23 12:46:05 +0100

    Добавить новые сообщения о полете дронов и улучшить вывод информации о приземлении

--- Program.cs
+++ Program.cs
@@ -12,7 +12,12 @@ void Runway(string name, int speed)
     for (int i = 0; i < maxCheckpoints; i++)
     {
         Console.WriteLine($"{name} reached checkpoint {i}.");
+        Thread.Sleep(640);
+        Console.WriteLine($"{name} is flying...");
     }
+    Console.WriteLine($"Drone {name} has landed successfully.");
 }
 
 Runway("Alpha", 50);
+Runway("Bravo", 70);
+Runway("Charlie", 60);
