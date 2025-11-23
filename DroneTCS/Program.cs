d5dc1ee52201c6eb4609c07298b84676a09345d8
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-11-23 18:28:08 +0100

    Refactor drone monitoring logic to use TaskCompletionSource and improve code structure

--- DroneTCS/Program.cs
+++ DroneTCS/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//task,TaskCompletionSource,taskwhwnall.Task.WhenAll 
+
+TaskCompletionSource<bool> DroneBrain = new TaskCompletionSource<bool>();
+TaskCompletionSource<bool> DroneMichel = new TaskCompletionSource<bool>();
+
+Task FlyDrone(string name, int speed)
+{
+    var monitorCheckpoints = 10;
+    for (int i = 1; i < monitorCheckpoints; i++)
+    {
+        Console.WriteLine($"Monitoring checkpoint {i} for drone {name}.");
+        Thread.Sleep(640);
+    }
+    FlyDrone("Brain", 100);
+    return Task.CompletedTask;
+}
+
+
+
+
--- Program.cs
+++ Program.cs
@@ -9,40 +9,6 @@ using System.Threading.Tasks;
 
 
 
-Console.WriteLine("1 - Thread + Join");
-Console.WriteLine("2 - Task + TCS");
-Console.WriteLine("3 - async/await");
-Console.WriteLine("4 - API");
-
-var input = Console.ReadLine();
-
-
-switch (input)
-{
-    case
-        "1":
-        PartA();
-        break;
-
-    case
-        "2":
-        PartB();
-        break;
-    case
-        "3":
-        PartC();
-        break;
-    case
-        "4":
-        PartD();
-        break;
-    default:
-        Console.WriteLine("404");
-        break;
-}
-
-
-
 
 
 
@@ -68,80 +34,43 @@ switch (input)
 
 
 
-void PartA()
+
+
+var t = new Thread(() =>
 {
-
-    var t = new Thread(() =>
+    string name = "Alpha";
+    int speed = 50;
+    Thread.Sleep(2000);
+    Console.WriteLine($"Monitoring drone operations for {name} at speed {speed} km/h...");
+    var monitorCheckpoints = 10;
+    for (int i = 0; i < monitorCheckpoints; i++)
     {
-        string name = "Alpha";
-        int speed = 50;
-        Thread.Sleep(2000);
-        Console.WriteLine($"Monitoring drone operations for {name} at speed {speed} km/h...");
-        var monitorCheckpoints = 10;
-        for (int i = 0; i < monitorCheckpoints; i++)
-        {
-            Console.WriteLine($"Monitoring checkpoint {i} for drone {name}.");
-            Thread.Sleep(640);
-        }
-        Console.WriteLine($"Monitoring completed for drone {name}.");
-    });
-
-    var t2 = new Thread(() =>
-    {
-        string name = "Bravo";
-        int speed = 70;
-        Thread.Sleep(2000);
-        Console.WriteLine($"Monitoring drone operations for {name} at speed {speed} km/h...");
-        var monitorCheckpoints = 10;
-        for (int i = 0; i < monitorCheckpoints; i++)
-        {
-            Console.WriteLine($"Monitoring checkpoint {i} for drone {name}.");
-            Thread.Sleep(640);
-        }
-        Console.WriteLine($"Monitoring completed for drone {name}.");
-    });
-
-
-    t2.Start();
-    t2.Join();
-
-    t.Start();
-    t.Join();
-    Console.WriteLine("All drones finished");
-
-}
-
-void PartB()
-
-//task,TaskCompletionSource,taskwhwnall.Task.WhenAll 
-{
-    TaskCompletionSource<bool> DroneBrain = new TaskCompletionSource<bool>();
-    TaskCompletionSource<bool> DroneMichel = new TaskCompletionSource<bool>();
-
-    Task FlyDrone(string name, int speed)
-    {
-        var monitorCheckpoints = 10;
-        for (int i = 1; i < monitorCheckpoints; i++)
-        {
-            Console.WriteLine($"Monitoring checkpoint {i} for drone {name}.");
-            Thread.Sleep(640);
-        }
-        FlyDrone("Brain", 100);
-        return Task.CompletedTask;
+        Console.WriteLine($"Monitoring checkpoint {i} for drone {name}.");
+        Thread.Sleep(640);
     }
+    Console.WriteLine($"Monitoring completed for drone {name}.");
+});
 
-
-}
-
-
-void PartC()
+var t2 = new Thread(() =>
 {
+    string name = "Bravo";
+    int speed = 70;
+    Thread.Sleep(2000);
+    Console.WriteLine($"Monitoring drone operations for {name} at speed {speed} km/h...");
+    var monitorCheckpoints = 10;
+    for (int i = 0; i < monitorCheckpoints; i++)
+    {
+        Console.WriteLine($"Monitoring checkpoint {i} for drone {name}.");
+        Thread.Sleep(640);
+    }
+    Console.WriteLine($"Monitoring completed for drone {name}.");
+});
 
-}
 
-void PartD()
-{
+t2.Start();
+t2.Join();
 
-}
-
+t.Start();
+t.Join();
+Console.WriteLine("All drones finished");
 
