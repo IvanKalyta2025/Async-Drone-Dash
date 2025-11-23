f0d58b83daaf0a2465d63f52f89538c2fb8e5929
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-11-23 19:15:43 +0100

    Refactor PartB method to improve drone flight monitoring logic and exception handling

--- DroneTCS/Program.cs
+++ DroneTCS/Program.cs
@@ -1,41 +1,61 @@
 ﻿//task,TaskCompletionSource,taskwhwnall.Task.WhenAll 
-async void PartB()
+
+TaskCompletionSource<bool> DroneBrain = new TaskCompletionSource<bool>();
+TaskCompletionSource<bool> DroneMichelle = new TaskCompletionSource<bool>();
+
+async Task FlyDrone(string name, int speed, TaskCompletionSource<bool> tcs)
 {
-    TaskCompletionSource<bool> DroneBrain = new TaskCompletionSource<bool>();
-    TaskCompletionSource<bool> DroneMichelle = new TaskCompletionSource<bool>();
+    Console.WriteLine($"Drone {name}.Speed: {speed} м/с has started its flight.");
 
-    async Task FlyDrone(string name, int speed, TaskCompletionSource<bool> tcs)
+    var monitorCheckpoints = 10;
+
+    try
     {
-        Console.WriteLine($"Drone {name}.Speed: {speed} м/с has started its flight.");
-
-        var monitorCheckpoints = 10;
-
-        try
+        for (int i = 1; i < monitorCheckpoints; i++)
         {
-            for (int i = 1; i < monitorCheckpoints; i++)
-            {
-                Console.WriteLine($"Monitoring checkpoint {i} for drone {name}.");
-                await Task.Delay(640);
-                Console.WriteLine($"{name} reached checkpoint {i}");
+            Console.WriteLine($"Monitoring checkpoint {i} for drone {name}.");
+            await Task.Delay(640);
+            Console.WriteLine($"{name} reached checkpoint {i}");
 
-                if (name == "Michelle" && i == 3)
-                {
-                    throw new Exception($"FATAL ERROR!!! Engine Failure on Drone {name} at Checkpoint {i}. Aborting mission.");
-                }
+            if (name == "Michelle" && i == 3)
+            {
+                throw new Exception($"FATAL ERROR!!! Engine Failure on Drone {name} at Checkpoint {i}. Aborting mission.");
             }
         }
-        catch (Exception fatal_error)
-        {
-            Console.WriteLine($" [ERROR] Drone {name} caught an exception.");
-            tcs.SetException(fatal_error);
-        }
-
-        // FlyDrone("Brain", 100);
-        // return Task.CompletedTask;
+        Console.WriteLine($"Drone {name} has completed its route!");
+        tcs.SetResult(true);
     }
-    Task flightBrian = FlyDrone("Brian", 100, DroneBrain);
-    Task flightMichelle = FlyDrone("Michelle", 80, DroneMichelle);
+    catch (Exception fatal_error)
+    {
+        Console.WriteLine($" [ERROR] Drone {name} caught an exception.");
+        tcs.SetException(fatal_error);
+    }
+
+    // FlyDrone("Brain", 100);
+    // return Task.CompletedTask;
 }
+Task flightBrian = FlyDrone("Brian", 100, DroneBrain);
+Task flightMichelle = FlyDrone("Michelle", 80, DroneMichelle);
+
+Task allFlightsCompleted = Task.WhenAll(DroneBrain.Task, DroneMichelle.Task);
+
+try
+{
+    await allFlightsCompleted;
+}
+
+catch
+{
+    if (allFlightsCompleted.Exception != null)
+    {
+        // Task.Exception является AggregateException, который содержит все ошибки
+        foreach (var innerEx in allFlightsCompleted.Exception.InnerExceptions)
+        {
+            Console.WriteLine($"Detailed Failure: {innerEx.Message}");
+        }
+    }
+}
+await Task.WhenAll(flightBrian, flightMichelle);
 
 
 
