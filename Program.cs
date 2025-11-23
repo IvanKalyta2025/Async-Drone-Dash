8f120d02264a4a3340459f5a48d270cd813d9560
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-11-23 12:37:28 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.Data;
+using System.Net.Security;
+
+void Runway(string name, int speed)
+{
+    Console.WriteLine($"Drone {name} is taking off at speed {speed} km/h.");
+
+    var maxCheckpoints = 10;
+
+    for (int i = 0; i < maxCheckpoints; i++)
+    {
+        Console.WriteLine($"{name} reached checkpoint {i}.");
+    }
+}
