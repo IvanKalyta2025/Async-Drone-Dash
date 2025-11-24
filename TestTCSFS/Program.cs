398eb53c115cb785f5556741b20ccd57fccf9c2e
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-11-24 10:09:59 +0100

    editing TestEx

--- TestTCSFS/Program.cs
+++ TestTCSFS/Program.cs
@@ -6,6 +6,15 @@ namespace AsyncTcsExample
 
     public class PickapOrder
     {
+        public TaskCompletionSource<int> _orderFs = new TaskCompletionSource<int>();
+
+        //у нас есть какой то заказ и нам нужно понять где находиться наш почтовый чел.
+        public Task<int> WhereIsPostman => _orderFs.Task;
+
+        public void StartOfDelivery(string whereispostman)
+        {
+
+        }
 
     }
 
